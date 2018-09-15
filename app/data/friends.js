ad11738a0599ae2e00679033653f4b00d33fe9c6
JMac <julia@defmethod.com>
2018-09-15 15:17:58 -0400

    Updates to Survey.html and friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,11 +1,9 @@
 var friendArray = [
-    {
-      userName: "Julia",
-      userEmail: "julia@gmail.com",
-      userCuddleScore: 20,
-      userFriendlyScore: 30,
-      userCreepyScore: 5,
-    } 
-  ];
+  {
+    name: "Julia",
+    photo: "https://media.licdn.com/dms/image/C4D03AQExZTa82rGAuA/profile-displayphoto-shrink_200_200/0?e=1542240000&v=beta&t=SW_ijUWvPqkkN8jz3gp_qMyq4jOlhkx23zVxGzT6tco",
+    scores: [5,1,4,4,5,1]
+  }
+];
 
   module.exports = friendArray;
