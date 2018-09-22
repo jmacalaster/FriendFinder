a685143e74b903f5366bc50640be6c551e57e60d
JMac <julia@defmethod.com>
2018-09-22 10:06:43 -0400

    Module Working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,52 @@
 var friendArray = [
   {
-    name: "Julia",
-    photo: "https://media.licdn.com/dms/image/C4D03AQExZTa82rGAuA/profile-displayphoto-shrink_200_200/0?e=1542240000&v=beta&t=SW_ijUWvPqkkN8jz3gp_qMyq4jOlhkx23zVxGzT6tco",
+    name: "Rex",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Puppy_on_Halong_Bay.jpg/281px-Puppy_on_Halong_Bay.jpg",
     scores: [5,1,4,4,5,1]
+  },
+
+  {
+    name: "Walker",
+    photo: "https://brightcove04pmdo-a.akamaihd.net/4221396001/4221396001_5209480649001_5209479447001-vs.jpg?pubId=4221396001&videoId=5209479447001",
+    scores: [5,5,5,5,5,5]
+  },
+
+  {
+    name: "Daisy",
+    photo: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2018/05/22224952/beagle-puppy-in-large-cushion-chair.jpg",
+    scores: [5,4,5,5,1,3]
+  },
+
+  {
+    name: "Porky",
+    photo: "https://i.pinimg.com/236x/4a/60/38/4a603827db8c421e7c272c9b6ddf3ec0--stuffed-pig-stuffed-animals.jpg",
+    scores: [2,4,5,1,5,1]
+  },
+
+  {
+    name: "Frank",
+    photo: "http://www.thepigsite.com/uploads/files/articles/suprisingPigFacts_fact7-image.jpg",
+    scores: [1,1,1,1,5,1]
+  },
+
+  {
+    name: "Skippy",
+    photo: "https://www.catster.com/wp-content/uploads/2015/06/snuggle_01.jpg",
+    scores: [2,4,1,5,5,1]
+  },
+
+  {
+    name: "Mina",
+    photo: "http://www.catsvscancer.org/wp-content/uploads/2015/03/cat-kitty-kitten-snuggling-cuddle.jpg",
+    scores: [4,3,2,4,5,1]
+  },
+
+  {
+    name: "Tiger",
+    photo: "https://photos.travellerspoint.com/426759/large_DSC09608.jpg",
+    scores: [1,5,5,4,2,1]
   }
+
 ];
 
   module.exports = friendArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,8 +9,35 @@ module.exports = function(app) {
         res.json(friendData);
       });
     
-    app.post("/api/friends", function(req, res) {        
+    app.post("/api/friends", function(req, res) {
+        var bestCuddle = {
+            name: "",
+            photo: ""  
+        };
+        var lowestDiff = Infinity;
+
+        var userInfo = req.body;
+
+        var userScores = userInfo.scores;
+
+        for (var i=0; i<friendData.length; i++) {
+            var currentFriend = friendData[i];
+            var currentFriendDiff = 0;
+            for (var j=0; j<currentFriend.scores.length; j++){
+                var userScoreOne = userScores[j];
+                var currentFriendScoreOne = currentFriend.scores[j];
+                currentFriendDiff += Math.abs(userScoreOne - currentFriendScoreOne);
+                console.log(currentFriendDiff);
+            }
+            
+            if (currentFriendDiff < lowestDiff){
+                bestCuddle.name = currentFriend.name;
+                bestCuddle.photo = currentFriend.photo;
+            }
+        console.log(bestCuddle);
+        }
+
         friendData.push(req.body);
-          res.json(true);
+          res.json(bestCuddle);
       });
 }
